e81eed735ee00475bd2d249664d828e19973b700
Skyler Reimer <skyler@stripe.com>
2021-09-14 19:22:35 -0700

    update CheckoutActivity in java

--- client/android/app/src/main/java/com/example/app/CheckoutActivityJava.java
+++ client/android/app/src/main/java/com/example/app/CheckoutActivityJava.java
@@ -1,33 +1,24 @@
 package com.example.app;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.stripe.android.ApiResultCallback;
-import com.stripe.android.SetupIntentResult;
-import com.stripe.android.Stripe;
+import com.stripe.android.PaymentConfiguration;
 import com.stripe.android.model.ConfirmSetupIntentParams;
 import com.stripe.android.model.PaymentMethod;
 import com.stripe.android.model.PaymentMethodCreateParams;
-import com.stripe.android.model.SetupIntent;
+import com.stripe.android.payments.paymentlauncher.PaymentLauncher;
+import com.stripe.android.payments.paymentlauncher.PaymentResult;
 import com.stripe.android.view.CardInputWidget;
 
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.lang.reflect.Type;
 import java.util.Map;
 
@@ -53,12 +44,19 @@ public class CheckoutActivityJava extends AppCompatActivity {
     private static final String backendUrl = "http://10.0.2.2:4242/";
     private OkHttpClient httpClient = new OkHttpClient();
     private String setupIntentClientSecret;
-    private Stripe stripe;
+    private PaymentLauncher paymentLauncher;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        final PaymentConfiguration paymentConfiguration = PaymentConfiguration.getInstance(getApplicationContext());
+        paymentLauncher = PaymentLauncher.Companion.create(
+                this,
+                paymentConfiguration.getPublishableKey(),
+                paymentConfiguration.getStripeAccountId(),
+                this::onPaymentResult
+        );
         loadPage();
     }
 
@@ -75,8 +73,7 @@ public class CheckoutActivityJava extends AppCompatActivity {
                     @Override
                     public void onFailure(Call call, IOException e) {
                         runOnUiThread(() -> {
-                            Context applicationContext = getApplicationContext();
-                            Toast.makeText(applicationContext, "Error: " + e.toString(), Toast.LENGTH_LONG).show();
+                            displayAlert("Error", e.toString(), true);
                         });
                     }
 
@@ -84,20 +81,14 @@ public class CheckoutActivityJava extends AppCompatActivity {
                     public void onResponse(Call call, final Response response) throws IOException {
                         if (!response.isSuccessful()) {
                             runOnUiThread(() -> {
-                                Context applicationContext = getApplicationContext();
-                                Toast.makeText(applicationContext, "Error: " + response.toString(), Toast.LENGTH_LONG).show();
+                                displayAlert("Error", response.toString(), true);
                             });
                         } else {
                             Gson gson = new Gson();
                             Type type = new TypeToken<Map<String, String>>(){}.getType();
                             Map<String, String> responseMap = gson.fromJson(response.body().string(), type);
 
-                            // The response from the server includes the Stripe publishable key and
-                            // SetupIntent details.
                             setupIntentClientSecret = responseMap.get("clientSecret");
-
-                            // Use the key from the server to initialize the Stripe instance.
-                            stripe = new Stripe(getApplicationContext(), responseMap.get("publishableKey"));
                         }
                     }
                 });
@@ -121,77 +112,42 @@ public class CheckoutActivityJava extends AppCompatActivity {
                         .create(card, billingDetails);
                 ConfirmSetupIntentParams confirmParams = ConfirmSetupIntentParams
                         .create(paymentMethodParams, setupIntentClientSecret);
-                stripe.confirmSetupIntent(this, confirmParams);
+                paymentLauncher.confirm(confirmParams);
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        WeakReference<Activity> weakActivity = new WeakReference<>(this);
+    private void onPaymentResult(PaymentResult paymentResult) {
+        String title = "";
+        String message = "";
+        boolean restartDemo = false;
+        if (paymentResult instanceof PaymentResult.Completed) {
+            title = "Setup Completed";
+            restartDemo = true;
+        } else if (paymentResult instanceof PaymentResult.Canceled) {
+            title = "Setup Canceled";
+        } else if (paymentResult instanceof PaymentResult.Failed) {
+            title = "Setup Failed";
+            message = ((PaymentResult.Failed) paymentResult).getThrowable().getMessage();
+        }
+        displayAlert(title, message, restartDemo);
+    }
 
-        // Handle the result of stripe.confirmSetupIntent
-        stripe.onSetupResult(requestCode, data, new ApiResultCallback<SetupIntentResult>() {
-            @Override
-            public void onSuccess(@NonNull SetupIntentResult result) {
-                SetupIntent setupIntent = result.getIntent();
-                SetupIntent.Status status = setupIntent.getStatus();
-                if (status == SetupIntent.Status.Succeeded) {
-                    // Setup completed successfully
-                    runOnUiThread(() -> {
-                        if (weakActivity.get() != null) {
-                            Activity activity = weakActivity.get();
-                            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                            builder.setTitle("Setup completed");
-                            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                            builder.setMessage(gson.toJson(setupIntent));
-                            builder.setPositiveButton("Restart demo", (DialogInterface dialog, int index) -> {
-                                CardInputWidget cardInputWidget = findViewById(R.id.cardInputWidget);
-                                cardInputWidget.clear();
-                                EditText emailInput = findViewById(R.id.emailInput);
-                                emailInput.setText(null);
-                                loadPage();
-                            });
-                            AlertDialog dialog = builder.create();
-                            dialog.show();
-                        }
-                    });
-                } else if (status == SetupIntent.Status.RequiresPaymentMethod) {
-                    // Setup failed – allow retrying using a different payment method
-                    runOnUiThread(() -> {
-                        Activity activity = weakActivity.get();
-                        if (activity != null) {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                            builder.setTitle("Setup failed");
-                            builder.setMessage(setupIntent.getLastSetupError().getMessage());
-                            builder.setPositiveButton("Ok", (DialogInterface dialog, int index) -> {
-                                CardInputWidget cardInputWidget = findViewById(R.id.cardInputWidget);
-                                cardInputWidget.clear();
-                                EditText emailInput = findViewById(R.id.emailInput);
-                                emailInput.setText(null);
-                            });
-                            AlertDialog dialog = builder.create();
-                            dialog.show();
-                        }
-                    });
-                }
-            }
-
-            @Override
-            public void onError(@NonNull Exception e) {
-                // Setup request failed – allow retrying using the same payment method
-                runOnUiThread(() -> {
-                    Activity activity = weakActivity.get();
-                    if (activity != null) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                        builder.setMessage(e.toString());
-                        builder.setPositiveButton("Ok", null);
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    }
+    private void displayAlert(String title, String message, Boolean restartDemo) {
+        runOnUiThread(()-> {
+            final CardInputWidget cardInputWidget= findViewById(R.id.cardInputWidget);
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                    .setTitle(title)
+                    .setMessage(message);
+            if (restartDemo) {
+                builder.setPositiveButton("Restart Demo", (v1, v2) -> {
+                    cardInputWidget.clear();
+                    loadPage();
                 });
+            } else {
+                builder.setPositiveButton("Ok", null);
             }
+            builder.create().show();
         });
     }
 }
